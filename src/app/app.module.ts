29a34988d0c798f64aece011bd7b03c061fb1c7e
Praveen <praveen@example.com>
2020-03-25 18:58:30 +0530

    created cols

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TcsListingComponent } from './tcs-listing/tcs-listing.component';
 import { TcsFormComponent } from './tcs-form/tcs-form.component';
+import { HeaderComponent } from './common/header/header.component';
+import { LeftColComponent } from './common/left-col/left-col.component';
+import { RightColComponent } from './common/right-col/right-col.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TcsListingComponent,
-    TcsFormComponent
+    TcsFormComponent,
+    HeaderComponent,
+    LeftColComponent,
+    RightColComponent
   ],
   imports: [
     BrowserModule,
